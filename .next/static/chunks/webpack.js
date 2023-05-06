c4c8db2d3882fb62b415b58beeba8ebea91103ac
Phoenix-Dev2 <116957217+Phoenix-Dev2@users.noreply.github.com>
2023-05-06 03:20:08 +0300

    gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "846bd6178b7f3460"; }
+/******/ 		__webpack_require__.h = function() { return "22aa1d50f953a034"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.09b25041b55d16c7.hot-update.js
+++ .next/static/webpack/webpack.09b25041b55d16c7.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "846bd6178b7f3460"; }
+/******/ 	__webpack_require__.h = function() { return "22aa1d50f953a034"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
